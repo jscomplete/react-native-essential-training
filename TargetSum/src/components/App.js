98b059ecf559671526a344c7bb8b21a0ef81092d
Samer Buna <samer.buna@gmail.com>
2017-09-30 16:14:32 -0700

    3.3 - Configure eslint

--- TargetSum/src/components/App.js
+++ TargetSum/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 class App extends React.Component {
   render() {
