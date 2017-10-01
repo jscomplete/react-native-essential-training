60b380833dfff05d68720ca84cf65ddfd8f8453d
Samer Buna <samer.buna@gmail.com>
2017-09-30 22:14:58 -0700

    3.15 - Add the Play Again button to mount a new game

--- TargetSum/src/components/App.js
+++ TargetSum/src/components/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import Game from './Game';
 
 class App extends React.Component {
+  state = {
+    gameId: 1,
+  };
+  resetGame = () => {
+    this.setState((prevState) => {
+      return { gameId: prevState.gameId + 1 };
+    });
+  };
   render() {
     return (
-      <Game randomNumberCount={6} initialSeconds={10} />
+      <Game
+        key={this.state.gameId}
+        onPlayAgain={this.resetGame}
+        randomNumberCount={6}
+        initialSeconds={10}
+      />
     );
   }
 }
