a965cdf15ca72bcc2014dbc82f5aad6ccd6834dd
Samer Buna <samer.buna@gmail.com>
2017-10-01 08:49:24 -0700

    4.2 - Initialize the empty Bakesale app

---
+++ Bakesale/src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Bakesale</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 40,
+  },
+});
+
+export default App;
