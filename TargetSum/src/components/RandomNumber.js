c62ebcfa21f541b9c16e6e301c06d1e50553a21c
Samer Buna <samer.buna@gmail.com>
2017-09-30 17:59:27 -0700

    3.7 - Surround play numbers with TouchableOpacity

--- TargetSum/src/components/RandomNumber.js
+++ TargetSum/src/components/RandomNumber.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import { Text, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 class RandomNumber extends React.Component {
   static propTypes = {
     number: PropTypes.number.isRequired,
   };
+  handlePress = () => {
+    // console.log(this.props.number);
+  };
   render() {
     return (
-      <Text style={styles.random}>{this.props.number}</Text>
+      <TouchableOpacity onPress={this.handlePress}>
+        <Text style={styles.random}>{this.props.number}</Text>
+      </TouchableOpacity>
     );
   }
 }
