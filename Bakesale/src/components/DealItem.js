01fd957ea23af8e0920cfc943bb269b473313ec8
Samer Buna <samer.buna@gmail.com>
2020-08-30 20:21:32 -0700

    4.6 - Styling for DealItem

--- Bakesale/src/components/DealItem.js
+++ Bakesale/src/components/DealItem.js
@@ -11,15 +11,14 @@ class DealItem extends React.Component {
   render() {
     const { deal } = this.props;
     return (
-      <View>
-        <Image
-          source={{ uri: deal.media[0] }}
-          style={styles.image}
-        />
-        <View>
-          <Text>{deal.title}</Text>
-          <Text>{priceDisplay(deal.price)}</Text>
-          <Text>{deal.cause.name}</Text>
+      <View style={styles.deal}>
+        <Image source={{ uri: deal.media[0] }} style={styles.image} />
+        <View style={styles.info}>
+          <Text style={styles.title}>{deal.title}</Text>
+          <View style={styles.footer}>
+            <Text style={styles.cause}>{deal.cause.name}</Text>
+            <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+          </View>
         </View>
       </View>
     );
@@ -27,9 +26,36 @@ class DealItem extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  deal: {
+    marginHorizontal: 12,
+    marginTop: 12,
+  },
   image: {
     width: '100%',
     height: 150,
+    backgroundColor: '#ccc',
+  },
+  info: {
+    padding: 10,
+    backgroundColor: '#fff',
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderTopWidth: 0,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  cause: {
+    flex: 2,
+  },
+  price: {
+    flex: 1,
+    textAlign: 'right',
   },
 });
 
