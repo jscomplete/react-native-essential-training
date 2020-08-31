c9ac2617216a061b84c421d72c6488020e85680a
Samer Buna <samer.buna@gmail.com>
2020-08-30 20:21:17 -0700

    3.4 - Style the target number

--- TargetSum/src/components/Game.js
+++ TargetSum/src/components/Game.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 class Game extends React.Component {
+  target = 10 + Math.floor(40 * Math.random());
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.target}>42</Text>
+        <Text style={styles.target}>{this.target}</Text>
       </View>
     );
   }
@@ -16,6 +17,14 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#ddd',
     flex: 1,
+    paddingTop: 30,
+  },
+
+  target: {
+    fontSize: 42,
+    backgroundColor: '#aaa',
+    marginHorizontal: 50,
+    textAlign: 'center',
   },
 });
 
