b97207ed2b7c36cac527bcdfc2207755e77f92d4
Samer Buna <samer.buna@gmail.com>
2020-08-30 20:21:18 -0700

    3.6 - Style the random play numbers

--- TargetSum/src/components/Game.js
+++ TargetSum/src/components/Game.js
@@ -13,13 +13,16 @@ class Game extends React.Component {
   target = this.randomNumbers
     .slice(0, this.props.randomNumberCount - 2)
     .reduce((acc, curr) => acc + curr, 0);
+  // TODO: Shuffle the random numbers
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.target}>{this.target}</Text>
-        {this.randomNumbers.map((randomNumber, index) =>
-          <Text key={index}>{randomNumber}</Text>
-        )}
+        <View style={styles.randomContainer}>
+          {this.randomNumbers.map((randomNumber, index) =>
+            <Text style={styles.random} key={index}>{randomNumber}</Text>
+          )}
+        </View>
       </View>
     );
   }
@@ -29,13 +32,28 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#ddd',
     flex: 1,
-    paddingTop: 30,
   },
 
   target: {
-    fontSize: 42,
-    backgroundColor: '#aaa',
-    marginHorizontal: 50,
+    fontSize: 50,
+    backgroundColor: '#bbb',
+    margin: 50,
+    textAlign: 'center',
+  },
+
+  randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+
+  random: {
+    backgroundColor: '#999',
+    width: 100,
+    marginHorizontal: 15,
+    marginVertical: 25,
+    fontSize: 35,
     textAlign: 'center',
   },
 });
