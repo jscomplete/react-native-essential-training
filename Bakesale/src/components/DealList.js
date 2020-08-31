9cc68b53ccd878141c8c5d44c0ae7248ae71c0a9
Samer Buna <samer.buna@gmail.com>
2020-08-30 20:21:30 -0700

    4.3 - Fetch initial list of deals

---
+++ Bakesale/src/components/DealList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+class DealList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Deals...</Text>
+      </View>
+    );
+  }
+}
+
+export default DealList;
