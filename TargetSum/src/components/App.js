2ccabfbeb0f69f9983462c98de96087aa00517d9
Samer Buna <samer.buna@gmail.com>
2020-08-30 20:21:17 -0700

    3.4 - Create the initial Game component (before styling)

--- TargetSum/src/components/App.js
+++ TargetSum/src/components/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-
-import { View, StyleSheet } from 'react-native';
+import Game from './Game';
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-      </View>
+      <Game />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ddd',
-    flex: 1,
-  }
-});
 
 export default App;
