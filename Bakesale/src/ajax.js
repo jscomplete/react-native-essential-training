92048436259bb1dab003cae5e1b355d74cdcfd08
Samer Buna <samer.buna@gmail.com>
2020-08-30 20:21:33 -0700

    4.7 - Fetch full deal information (before styling)

--- Bakesale/src/ajax.js
+++ Bakesale/src/ajax.js
@@ -3,8 +3,17 @@ const apiHost = 'https://bakesaleforgood.com';
 export default {
   async fetchInitialDeals() {
     try {
-      let response = await fetch(apiHost + '/api/deals');
-      let responseJson = await response.json();
+      const response = await fetch(apiHost + '/api/deals');
+      const responseJson = await response.json();
+      return responseJson;
+    } catch(error) {
+      console.error(error);
+    }
+  },
+  async fetchDealDetail(dealId) {
+    try {
+      const response = await fetch(apiHost + '/api/deals/' + dealId);
+      const responseJson = await response.json();
       return responseJson;
     } catch(error) {
       console.error(error);
